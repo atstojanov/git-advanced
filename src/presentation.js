4040191fc9d6d12f416d47c78fe10798be1bae1d
Angel Stoyanov <atstojanov@gmail.com>
2018-12-12 11:50:19 +0200

    Added Slides for all themes.

--- src/presentation.js
+++ src/presentation.js
@@ -19,10 +19,7 @@ import { CodeSlide, Title, CenteredList, Accent, theme } from './utils';
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck progress="bar" 
-      theme={theme}
-      transition={['slide']}
-      >
+      <Deck progress="bar" theme={theme} transition={['slide']}>
         <Slide id="title">
           <Title fit>
             Git Advanced
@@ -34,15 +31,28 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide id="outline">
           <Title>Outline</Title>
-          <List style={{listStyle: 'none'}}>
+          <List style={{ listStyle: 'none' }}>
             <ListItem>Merging vs. Rebasing</ListItem>
             <ListItem>Cherry Picking</ListItem>
             <ListItem>LFS (Large File Storage)</ListItem>
             <ListItem>Sub Modules</ListItem>
+            <ListItem>Tooling</ListItem>
           </List>
         </Slide>
-        <Slide id="merging_rebasing">
+        <Slide id="merging-rebasing">
           <Title>Merging vs. Rebasing</Title>
+        </Slide>
+        <Slide id="cherrypick">
+          <Title>Cherry Picking</Title>
+        </Slide>
+        <Slide id="lfs">
+          <Title>LFS (Large File Storage)</Title>
+        </Slide>
+        <Slide id="sub-modules">
+          <Title>Sub Modules</Title>
+        </Slide>
+        <Slide id="tools">
+          <Title>Tooling</Title>
         </Slide>
       </Deck>
     );
